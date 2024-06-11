48f8a82d6ee5550f2af03e56783a2f9e89df3568
Yguang <1791329809@qq.com>
2024-06-11 15:35:17 +0800

    初始化

---
+++ daocao_stater/src/main/java/com/stt/stater/DaocaoApplication.java
@@ -0,0 +1,11 @@
+package com.stt.stater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaocaoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DaocaoApplication.class, args);
+    }
+}
